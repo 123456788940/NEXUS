30a580eb36781624d0c78928751a5ea7f99f11af
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-02-06 13:29:54 +0530

    Create ICO.sol

---
+++ ICO.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ICO is ERC20{
+    address owner=0x17F6AD8Ef982297579C203069C1DbfFE4348c372;
+    modifier onlyOwner() {
+        require(owner==msg.sender, "only owner has access");
+        _;
+    }
+    uint public totalPool;
+    uint public initialSupply;
+    constructor(address _owner, uint _initialSupply) ERC20("Helix Token", "HLX"){
+        _mint(msg.sender, initialSupply);
+        initialSupply=_initialSupply;
+        owner=_owner;
+
+
+    }
+
+    mapping(address=>bool) public authenticated;
+     mapping(address=>bool) public isEnded;
+     mapping(address=>bool) public isDistributed;
+     address[] public payees;
+
+    function buyToken(uint amount) external onlyOwner {
+        require(amount>0, "amount has to be valid");
+         authenticated[owner] = true;
+         transferFrom(msg.sender, address(this), amount);
+         totalPool+=amount;
+
+    }
+      
+      
+        function endToken() external onlyOwner {
+          isEnded[owner] = true;
+        }
+
+        function distributeToken(address[] calldata receivers, uint amount) external {
+            for (uint i = 0; i < receivers.length; i++){
+         require(!isDistributed[receivers[i]], "distribution not doner");
+          isDistributed[receivers[i]] = true;
+          payees.push(receivers[i]);
+          transfer(receivers[i], amount);
+        }
+        }
+    
+}
