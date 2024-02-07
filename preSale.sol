27cf4b9a66de92ce847b0469b7d6814f1f25f451
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-02-07 15:25:04 +0530

    Update preSale.sol

--- preSale.sol
+++ preSale.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-import "./IERC.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PreSale {
     address public owner;
