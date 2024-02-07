3a10a8495f12654155bd1cde16418a7f28a41d25
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-02-07 14:30:20 +0530

    Update IERC.sol

--- IERC.sol
+++ IERC.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract IERC is ERC20 {
+    uint initialSupply;
+
+    constructor (uint _initialSupply) ERC20("Helix Token", "HLX") {
+        initialSupply = _initialSupply;
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(uint amount, address to) public {
+        _mint(to, amount);
+    }
+
+    function burn(uint amount, address to) public {
+        _burn(to, amount);
+    }
+
+    function trackToken() public view returns(uint) {
+        return trackToken();
+    }
+}
